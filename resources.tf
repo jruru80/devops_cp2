9f806518d44d7e0aedb820c11e569aed9a3bab59
Jruru80 <bonrubi@gmail.com>
2025-07-13 00:28:13 +0200

    feat: cambios

--- resources.tf
+++ resources.tf
@@ -1,50 +1,32 @@
+# Resource group de azure, de donde cuelgan los recursos
 resource "azurerm_resource_group" "vm" {
   name     = var.resource_group_name
   location = var.location_name
-}
 
-resource "azurerm_virtual_network" "vm" {
-  name                = var.network_name
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
-}
-
-resource "azurerm_subnet" "vm" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.vm.name
-  virtual_network_name = azurerm_virtual_network.vm.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "vm" {
-  name                = var.nic_name
-  location            = azurerm_resource_group.vm.location
-  resource_group_name = azurerm_resource_group.vm.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.vm.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm.id
+  tags = {
+    environment = "CP2"
   }
-  depends_on = [
-    azurerm_public_ip.vm
-  ]
 }
 
-resource "azurerm_public_ip" "vm" {
-  name                = "public_ip"
+resource "azurerm_network_security_group" "cp2SecGroup" {
+  name                = "sshtraffic"
   location            = azurerm_resource_group.vm.location
   resource_group_name = azurerm_resource_group.vm.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "CP2"
+  }
 }
 
-data "azurerm_public_ip" "vm" {
-  name                = azurerm_public_ip.vm.name
-  resource_group_name = azurerm_resource_group.vm.name
-  depends_on = [
-    azurerm_linux_virtual_machine.vm
-  ]
-}
